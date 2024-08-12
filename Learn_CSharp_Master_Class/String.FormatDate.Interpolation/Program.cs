e3d5768f89c5238ff8381eb4b60122f2ef0327f5
Gilang Agung Saputra <gilangagungs20@gmail.com>
2024-08-12 23:16:10 +0700

    Quiz: string interpolation and switch case

---
+++ Learn_CSharp_Master_Class/String.FormatDate.Interpolation/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace String.FormatDate.Interpolation
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Exercise.FormatDate(2023, 12, 1));
+        }
+    }
+
+    public class Exercise
+    {
+        public static string FormatDate(int year, int month, int day)
+        {
+            return string.Format("{0}/{1}/{2}", year, month, day);
+        }
+    }
+}
+
---
+++ Learn_CSharp_Master_Class/Switch.Case.DescribeDay/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Switch.Case.DescribeDay
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 8; i++)
+            {
+                Console.WriteLine(Exercise.DescribeDay(i));
+            }
+        }
+    }
+
+    class Exercise
+    {
+        /// <summary>
+        /// number 1-5 are Working Day, 6 and 7 are Weekend
+        /// </summary>
+        /// <param name="dayNumber"></param>
+        /// <returns></returns>
+        public static string DescribeDay(int dayNumber)
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    return "Working day";
+                break;
+                case 6:
+                case 7:
+                    return "Weekend";
+                break;
+                default:
+                    return "Invalid day number";
+            }
+        }
+    }
+}
+
