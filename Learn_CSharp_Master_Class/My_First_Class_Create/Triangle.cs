2a09d49b8df8aefdff012e8b30ec38a64be84036
Gilang Agung Saputra <gilang.saputra@formulatrix.com>
2025-04-24 17:30:53 +0700

    create class triangle then bypass base name

---
+++ Learn_CSharp_Master_Class/My_First_Class_Create/Triangle.cs
@@ -0,0 +1,22 @@
+﻿namespace My_First_Class_Create;
+
+public class Triangle
+{
+    private int @base;
+    private int height;
+    public Triangle( int @base, int height)
+    {
+        this.@base = @base;
+        this.height = height;
+    }
+
+    public double CalculateArea()
+    {
+        return ((@base * height) / 2);
+    }
+
+    public string AsString()
+    {
+        return $"Base is {@base}, height is {height}";
+    }
+}
