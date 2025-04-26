e9e2873b65a66c950096ebac30751104a8dab092
Gilang Agung Saputra <gilangagungs20@gmail.com>
2025-04-26 10:06:59 +0700

    quiz dog class

---
+++ Learn_CSharp_Master_Class/DogClassQuiz/Dog.cs
@@ -0,0 +1,43 @@
+﻿namespace DogClassQuiz;
+
+public class Dog
+{
+    private readonly string _name;
+    private readonly string _breed;
+    private readonly int _weight;
+    
+    public Dog( string name, string breed, int weight )
+    {
+        _name = name;
+        _breed = breed;
+        _weight = weight;
+    }
+
+    public Dog( string name, int weight)
+    {
+        _name = name;
+        _breed = "mixed-breed";
+        _weight = weight;
+    }
+
+    public string Describe()
+    {
+        return $"This dog is named {_name}, " +
+                         $"it's a {_breed}, " +
+                         $"and it weighs {_weight} kilograms, " +
+                         $"so it's a {Size(_weight)} dog.";
+    }
+
+    private string Size( int weight)
+    {
+        switch (weight)
+        {
+            case < 5:
+                return "tiny";
+            case < 30:
+                return "medium";
+            default:
+                return "large";
+        }
+    }
+}
---
+++ Learn_CSharp_Master_Class/DogClassQuiz/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DogClassQuiz;
+
+class Program
+{
+    static void Main()
+    {
+        Dog Lucky = new Dog("Lucky", "german shepherd", 40);
+        Dog Tina = new Dog("Tina", "shar pei", 25);
+        Dog Bobo = new Dog("Bobo", 3);
+
+        Console.WriteLine(Lucky.Describe());
+        Console.WriteLine(Tina.Describe());
+        Console.WriteLine(Bobo.Describe());
+    }
+}
