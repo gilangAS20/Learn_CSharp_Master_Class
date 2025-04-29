f93971bd77dd028972adad0404c73cd94575101f
Gilang Agung Saputra <gilang.saputra@formulatrix.com>
2025-04-29 19:40:18 +0700

    try apply this feature in c#

---
+++ Learn_CSharp_Master_Class/Learn_Classes/MedicalAppointment.cs
@@ -0,0 +1,30 @@
+﻿namespace Learn_Classes;
+
+public class MedicalAppointment
+{
+    private string _patientname;
+    private DateTime _date;
+    
+    public MedicalAppointment( string patientName, DateTime date)
+    {
+        _date = date;
+        _patientname = patientName;
+    }
+
+    public DateTime GetDate() => _date;
+
+    public void Reschedule(DateTime scheduleTime)
+    {
+        _date = scheduleTime;
+        AppointmentPrinter printer = new AppointmentPrinter();
+        printer.Print(this);
+    }
+}
+
+public class AppointmentPrinter
+{
+    public void Print( MedicalAppointment appointment )
+    {
+        Console.WriteLine($"appointment date is changed to: " + appointment.GetDate());
+    }
+}
---
+++ Learn_CSharp_Master_Class/Learn_Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Learn_Classes;
+
+class Program
+{
+    static void Main()
+    {
+        MedicalAppointment appointment1 = new MedicalAppointment("Gilang", new DateTime(2025, 1, 29));
+        appointment1.Reschedule(DateTime.Now);
+    }
+}
