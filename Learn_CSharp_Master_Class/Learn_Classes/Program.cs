cbf3bd016dfa92f99613d03e07b73fb24133db58
Gilang Agung Saputra <gilang.saputra@formulatrix.com>
2025-04-29 20:09:18 +0700

    try default value in ctor

--- Learn_CSharp_Master_Class/Learn_Classes/Program.cs
+++ Learn_CSharp_Master_Class/Learn_Classes/Program.cs
@@ -8,5 +8,10 @@ class Program
     {
         MedicalAppointment appointment1 = new MedicalAppointment("Gilang", new DateTime(2025, 1, 29));
         appointment1.Reschedule(DateTime.Now);
+        
+        
+        // rectangle with default value more than negative number
+        Rectangle rectangleNegative = new Rectangle(-10, -21);
+        Rectangle rectangleZero = new Rectangle(0, 0);
     }
 }
---
+++ Learn_CSharp_Master_Class/Learn_Classes/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿namespace Learn_Classes;
+
+public class Rectangle
+{
+    private readonly int _width;
+    private readonly int _height;
+    
+    public Rectangle( int width, int height )
+    {
+        _width = CheckForNegativeAndDefault(width, nameof(_width));
+        _height = CheckForNegativeAndDefault(height, nameof(_height));
+    }
+
+    private int CheckForNegativeAndDefault(int number, string paramName)
+    {
+        if ( number < 0 )
+        {
+            Console.WriteLine($"{paramName} must be 0 or greater, assign default to 0 (zero)");
+            return 0;
+        }
+
+        return number;
+    }
+}
