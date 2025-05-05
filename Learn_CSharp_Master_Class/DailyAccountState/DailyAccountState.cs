f959833d30abd55fa2b798cc504fc828a208d676
Gilang Agung Saputra <gilang.saputra@formulatrix.com>
2025-05-05 17:12:06 +0700

    continue quiz of calculated properties

---
+++ Learn_CSharp_Master_Class/DailyAccountState/DailyAccountState.cs
@@ -0,0 +1,21 @@
+﻿namespace DailyAccountStateClass;
+
+public class DailyAccountState
+{
+    public int InitialState { get; }
+        
+    public int SumOfOperations { get;}
+        
+    public DailyAccountState(
+        int initialState, 
+        int sumOfOperations)
+    {
+        InitialState = initialState;
+        SumOfOperations = sumOfOperations;
+    }
+
+    public int EndOfDayState => InitialState + SumOfOperations;
+
+    public string Report => $"Day: {DateTime.Now.Day}, month: {DateTime.Now.Month}, year: { DateTime.Now.Year}, "+
+                            $"initial state: {InitialState}, end of day state: {InitialState+SumOfOperations}";
+}
---
+++ Learn_CSharp_Master_Class/DailyAccountState/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DailyAccountStateClass;
+
+DailyAccountState dailyAccountState = new DailyAccountState(2000, -200);
+
+Console.WriteLine(dailyAccountState.EndOfDayState);
+Console.WriteLine(dailyAccountState.Report);
