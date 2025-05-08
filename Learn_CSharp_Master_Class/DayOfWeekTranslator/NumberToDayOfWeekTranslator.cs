8fe6bd010f74530be34ebba54c976eb099c378f0
Gilang Agung Saputra <gilang.saputra@formulatrix.com>
2025-05-08 17:56:18 +0700

    quiz: create static class

---
+++ Learn_CSharp_Master_Class/DayOfWeekTranslator/NumberToDayOfWeekTranslator.cs
@@ -0,0 +1,19 @@
+﻿namespace DayOfWeekTranslator;
+
+public static class NumberToDayOfWeekTranslator
+{
+    public static string Translate(int numOfDay)
+    {
+        return numOfDay switch
+        {
+            1 => "Monday",
+            2 => "Tuesday",
+            3 => "Wednesday",
+            4 => "Thursday",
+            5 => "Friday",
+            6 => "Saturday",
+            7 => "Sunday",
+            _ => "Invalid day of the week"
+        };
+    }
+}
